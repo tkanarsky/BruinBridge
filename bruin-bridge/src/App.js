db7a865e24f216082f5baf8029066ef81c0e8810
Ian Galvez <iangalvez@g.ucla.edu>
2020-05-04 16:48:08 -0700

    simple test - trivially modify app.js

--- bruin-bridge/src/App.js
+++ bruin-bridge/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Test: it's working on my end!
         </p>
         <a
           className="App-link"
